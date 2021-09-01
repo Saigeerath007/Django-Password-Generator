0ebc489661e216338cb930ea182a3969bc562b0d
SaiGeerath <saigeerath007@gmail.com>
2021-09-01 21:23:11 +0530

    3rd

--- generator/views.py
+++ generator/views.py
@@ -1,21 +1,31 @@
 from django.shortcuts import render
 from django.contrib import redirects
 from django.shortcuts import render
-
+import random
 
 # Create your views here.
 
 def home(request):
-    num = [i for i in range(0, 10)]
+    num = [str(i) for i in range(0, 10)]
     alpha = 'abcdefghijklmnopqrstuvwxyz'
     upper_char = list(alpha.upper())
     lower_char = list(alpha)
-    spec_char = list('!@#^&*;?<>')
-    return render(request, 'generator/home.html')
+    spec_char = list('!@#&;?')
+
+    l = upper_char + lower_char + spec_char + num
+    random.shuffle(l)
+    length = random.randint(8,15)
+    r=''
+    for i in range(0,length):
+        r += random.choice(l)
+
+    print(l)
+    return render(request, 'generator/home.html', context={'list' : r})
 
 
 def about(request):
     return render(request, 'generator/about.html')
 
+
 def error(request ,invalid_url):
-    return render(request, 'generator/home.html')
+    return render(request, 'generator/Invalid.html', context={'url' : invalid_url})
